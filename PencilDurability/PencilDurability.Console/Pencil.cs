a004a497366a86bd4d019366b8c75fccdab1283f
Jerel R Hass <jerel.hass@gmail.com>
2019-02-22 21:48:18 -0500

    Wrote a single character on a surface

--- PencilDurability/PencilDurability.Console/Pencil.cs
+++ PencilDurability/PencilDurability.Console/Pencil.cs
@@ -1,7 +1,17 @@
+using System.Linq;
+
 namespace PencilDurability.Console
 {
     public class Pencil
     {
-        
+        public void WriteOn(string text, ISurface surface)
+        {
+            surface.Write(text.First());
+        }
+    }
+    
+    public interface ISurface
+    {
+        void Write(char text);
     }
 }
--- PencilDurability/PencilDurability.Tests/PencilTests.cs
+++ PencilDurability/PencilDurability.Tests/PencilTests.cs
@@ -1,14 +1,24 @@
+using Moq;
 using PencilDurability.Console;
+using Xunit;
 
 namespace PencilDurability.Tests
 {
     public class PencilTests
     {
-        private Pencil sut;
+        private readonly Pencil _sut;
 
         public PencilTests()
         {
-            sut = new Pencil();
+            _sut = new Pencil();
+        }
+
+        [Fact]
+        public void GivenAPencilAndPaper_WhenInstructedToWriteASingleCharacterOfAString_ThenThePencilWillWriteOnThePaper()
+        {
+            var surfaceMoq = new Mock<ISurface>();
+            _sut.WriteOn("S", surfaceMoq.Object);
+            surfaceMoq.Verify(s => s.Write(It.Is<char>(text => text == 'S')), Times.Once);
         }
     }
 }
