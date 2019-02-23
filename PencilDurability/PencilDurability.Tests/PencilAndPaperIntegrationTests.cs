177f957c94e2cef07353ea0762ae4ca2ec6906c6
Jerel R Hass <jerel.hass@gmail.com>
2019-02-22 22:37:22 -0500

    Pencil Paper Integration Test

---
+++ PencilDurability/PencilDurability.Tests/PencilAndPaperIntegrationTests.cs
@@ -0,0 +1,17 @@
+using PencilDurability.Console;
+using Xunit;
+
+namespace PencilDurability.Tests
+{
+    public class PencilAndPaperIntegrationTests
+    {
+        [Fact]
+        public void GivenAPenAndPaperWithInitialText_WhenThePencilWritesAString_ThenTheTextCanBeSeenOnThePaper()
+        {
+            var pencil = new Pencil();
+            var paper = new Paper("She sells sea shells");
+            pencil.WriteOn(" down by the sea shore", paper);
+            Assert.Equal("She sells sea shells down by the sea shore", paper.Show());
+        }
+    }
+}
