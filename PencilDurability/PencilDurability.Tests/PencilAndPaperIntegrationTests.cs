9459e11b56e52b20e5257be60057ba3e65466f26
Jerel R Hass <jerel.hass@gmail.com>
2019-02-23 00:34:55 -0500

    Pencil Paper erase integration

--- PencilDurability/PencilDurability.Tests/PencilAndPaperIntegrationTests.cs
+++ PencilDurability/PencilDurability.Tests/PencilAndPaperIntegrationTests.cs
@@ -13,5 +13,14 @@ namespace PencilDurability.Tests
             pencil.WriteOn(" down by the sea shore", paper);
             Assert.Equal("She sells sea shells down by the sea shore", paper.Show());
         }
+        
+        [Fact]
+        public void GivenAPenAndPaperWithInitialText_WhenThePencilErasesAString_ThenTheTextIsReplaceWithSpacesOnThePaper()
+        {
+            var pencil = new Pencil();
+            var paper = new Paper("She sells sea shells");
+            pencil.EraseOn("shells", paper);
+            Assert.Equal("She sells sea       ", paper.Show());
+        }
     }
 }
