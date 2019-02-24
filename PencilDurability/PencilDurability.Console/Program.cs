b789faef5f3f73e71280e811a2fd7a4965d7a683
Jerel R Hass <jerel.hass@gmail.com>
2019-02-24 00:50:19 -0500

    Adding notes

--- PencilDurability/PencilDurability.Console/Program.cs
+++ PencilDurability/PencilDurability.Console/Program.cs
@@ -6,11 +6,7 @@ namespace PencilDurability.Console
     {
         static void Main(string[] args)
         {
-            //TODO: demonstrate the use of the pencil and paper through a standard text based game.
-            System.Console.WriteLine("You see a simple sheet of paper sitting on a desk. A pencil sits across the paper. There appear to be words written on the paper.");
-            System.Console.WriteLine("What would you like to do?");
-            System.Console.WriteLine("1) Read the paper");
-            System.Console.WriteLine("2) Write something with the pencil");
+            System.Console.WriteLine("For a more interesting experience checkout https://github.com/Agies/Pencil-Durability-CSharp/tree/feature/introduction_game");
         }
     }
 }
